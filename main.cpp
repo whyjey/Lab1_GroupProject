d74bc8ef79ec5064a1092019171cf2e018239207
Yaroslav Vasylenko <regenuss@gmail.com>
2026-09-17 21:44:59 +0300

    Implement Rugne-Kutta 4 method and update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,19 +16,33 @@ int main()
     const auto& [xValues, yValues, finalValue, error, executionTime] = *resultA;
 
     std::cout << "Student A - Euler method\n\n";
-
     std::cout << std::fixed << std::setprecision(6);
-
     std::cout << "x\t\ty\n";
-
     for (std::size_t i = 0; i < xValues.size(); ++i)
     {
         std::cout << xValues[i] << "\t\t" << yValues[i] << '\n';
     }
-
     std::cout << "\nFinal value: " << finalValue << '\n';
     std::cout << "Error: " << error << '\n';
     std::cout << "Execution time: " << executionTime << " microseconds\n";
+
+    std::cout << "--------------------------------------------------\n\n";
+
+    auto resultB = solveStudentB(input);
+    const auto& [
+        xValuesB, yValuesB,
+        finalValueB, errorB, executionTimeB
+    ] = *resultB;
+
+    std::cout << "Student B - Runge-Kutta 4 method\n\n";
+    std::cout << "x\t\ty\n";
+    for (std::size_t i = 0; i < xValuesB.size(); ++i)
+    {
+        std::cout << xValuesB[i] << "\t\t" << yValuesB[i] << '\n';
+    }
+    std::cout << "\nFinal value: " << finalValueB << '\n';
+    std::cout << "Error: " << errorB << '\n';
+    std::cout << "Execution time: " << executionTimeB << " microseconds\n";
 
     return 0;
 }
